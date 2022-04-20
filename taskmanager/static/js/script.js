cbbd09cb0b1b0fa6aab58f5df63b386124e4a737
Pedro Cristo <pcristo35@gmail.com>
2022-04-20 09:20:49 +0000

    Add script for date box modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,10 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-});
-
-// MODAL
-document.addEventListener('DOMContentLoaded', function() {
+  
+    // MODAL
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
-  });
+
+     // datepicker initialization
+     let datepicker = document.querySelectorAll(".datepicker");
+     M.Datepicker.init(datepicker, {
+         format: "dd mmmm, yyyy",
+         i18n: {done: "Select"}
+        });
+
+        // select initialization
+        let selects = document.querySelectorAll("select");
+        M.FormSelect.init(selects);
+});
